bf373dca52fc2030449fe4bc981fc70d5ec461bc
liujinyu <liujinyu-king@163.com>
2021-02-01 15:27:49 +0800

    升级

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -13,26 +13,33 @@ import java.util.*;
  * @modified：
  */
 @RestController
-@RequestMapping("Lottery")
+@RequestMapping("lottery")
 public class TestController {
-
-    @RequestMapping("testUrl/{num}")
-    public String chooseLottery(@PathVariable("num") int num) {
+    /**
+     * @info: 11选5生成号码
+     * @params: total  生成总组数
+	 * @params: num    每组个数
+     * @return: java.lang.String
+     * @author: liujinyu
+     * @create 2021/2/1
+     * @update
+     */
+    @RequestMapping("testUrl/{total}/{num}")
+    public String chooseLottery(@PathVariable("total") int total,@PathVariable("num") int num) {
         String s = "";
-        for (int i = 0; i < num; i++) {
-            s += elevenChooseFive() + "<br>";
+        for (int i = 0; i < total; i++) {
+            s += elevenChooseFive(num) + "<br>";
         }
         return s;
     }
-
-    private String elevenChooseFive() {
+    private String elevenChooseFive(int num) {
         List<Integer> list = new LinkedList<>();
         for (int i = 0; i < 11; i++) {
             list.add(i + 1);
         }
         Set<Integer> resultSet = new TreeSet<>();
         Random random = new Random();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < num; i++) {
             int index = random.nextInt(11 - i);
             int s = list.get(index);
             resultSet.add(s);
